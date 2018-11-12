e298032b5bfca99236758334cc95d444f82ab159
AdamSchon <adam.schon.92@gmail.com>
2018-11-12 12:37:58 +0100

    123

--- javastuff/MyDieTest.java
+++ javastuff/MyDieTest.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 //package Die;
 public class MyDieTest {
-  public static void main() {
+  public static void main(String [] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("How many sides should the die have?");
     int sides = sc.nextInt();
