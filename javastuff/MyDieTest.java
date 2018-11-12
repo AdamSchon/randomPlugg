124c28184dcf34aca3ec44685be0700043039a9f
AdamSchon <adam.schon.92@gmail.com>
2018-11-12 12:47:41 +0100

    123

--- javastuff/MyDieTest.java
+++ javastuff/MyDieTest.java
@@ -5,9 +5,10 @@ public class MyDieTest {
     Scanner sc = new Scanner(System.in);
     System.out.println("How many sides should the die have?");
     int sides = sc.nextInt();
-    Die d = new Die(sides);
+    Die d = new PairOfDice(sides);
     for (int i = 0; i < 10; i++) {
-      System.out.println("Die " + i  + " rolled " + d.roll() + ".");
+      d.roll();
+      System.out.println(d.toString());
     }
   }
 }
