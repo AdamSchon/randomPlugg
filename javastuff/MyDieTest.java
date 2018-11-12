f7c65e2b5be56308c9f79edd5e3887c38e0c1bb1
AdamSchon <adam.schon.92@gmail.com>
2018-11-12 12:53:53 +0100

    123

--- javastuff/MyDieTest.java
+++ javastuff/MyDieTest.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 //package Die;
 public class MyDieTest {
   public static void main(String [] args) {
+    /*
     Scanner sc = new Scanner(System.in);
     System.out.println("How many sides should the die have?");
     int sides = sc.nextInt();
@@ -10,5 +11,23 @@ public class MyDieTest {
       d.roll();
       System.out.println(d.toString());
     }
+    */
+    String a = "Beefheart";
+    String b = "Beefheart";
+    String c = "heart";
+    String d = "Beef" + c;
+    int i = 1;
+    int j = 1;
+    Integer k = 2;
+    Integer l = i + j;
+    System.out.println(a == b);
+    System.out.println(a.equals(b));
+    System.out.println(a == d);
+    System.out.println(a.equals(d));
+    System.out.println(i == j);
+    System.out.println(k == i + j);
+    System.out.println(k.equals(i+j));
+    System.out.println(k == l);
+    System.out.println(k.equals(l));
   }
 }
