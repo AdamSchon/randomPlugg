1097b8887443bf51d8902406e0e112da9fdf8ab4
AdamSchon <adam.schon.92@gmail.com>
2018-12-17 15:19:49 +0100

    123

--- uppgift2/Line.java
+++ uppgift2/Line.java
@@ -5,7 +5,7 @@ public class Line extends Edge {
   private int line;
 
   public Line(final Node n1, final Node n2, final int weight, int newLine) {
-    super(final Node n1, final Node n2, final int weight);
+    super(n1, n2, weight);
     this.line = newLine;
   }
 }
