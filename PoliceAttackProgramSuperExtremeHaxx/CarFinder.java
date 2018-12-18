d2e7f617baf48d0aaa20dda85f210014c53eccf8
AdamSchon <adam.schon.92@gmail.com>
2018-12-18 15:20:06 +0100

    1234

--- PoliceAttackProgramSuperExtremeHaxx/CarFinder.java
+++ PoliceAttackProgramSuperExtremeHaxx/CarFinder.java
@@ -30,7 +30,7 @@ public class CarFinder {
   public static void main(String [] args) {
     CarFinder CF = new CarFinder();
     for (int i = 0; i < 10; i++) {
-      Car newCar = new Car(Integer.toString(i), Math.round(Math.random()*10), Math.round(Math.random()*10));
+      Car newCar = new Car(Integer.toString(i), (int) Math.round(Math.random()*10), (int) Math.round(Math.random()*10));
       CF.addCar(newCar);
     }
 
