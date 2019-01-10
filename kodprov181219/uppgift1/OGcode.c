5e03afadb6b290de1f2fc96802a494f6f6ee3e5a
AdamSchon <adam.schon.92@gmail.com>
2019-01-10 15:41:06 +0100

    123

--- kodprov181219/uppgift1/OGcode.c
+++ kodprov181219/uppgift1/OGcode.c
@@ -1,6 +1,5 @@
 #include <string.h>
 #include <stdbool.h>
-#include <stdio.h>
 
 /// \returns true if c is a character in the string delimiters
 bool is_delimiter(int c, char *delimiters)
@@ -14,80 +13,38 @@ bool is_delimiter(int c, char *delimiters)
 /// \returns number of replacements (same as number of pairs) in replacements
 int ioopm_undo_strtok(char *original, int *replacements)
 {
-  int i = 0;
-  for (; ; i = i+2){
-    if (replacements[i] == '\0') {
-      break;
-    }
-    original[replacements[i+1]] = replacements[i];
-  }
-  return i/2;
+  /// Write your code here!
+  return -1; 
 }
 
-/// TODO: Implement in accordance with the specification
+/// TODO: Implement in accordance with the specification 
 /// \param src a null-terminated string or NULL if we want to continue tokenisation of the current string
 /// \param delimiters the delimiters used to tokenise src
 /// \param a pointer to replacements (see documentation in ioopm_undo_strtok) -- you can assume it has enough space
-/// \returns the next token in the string
+/// \returns the next token in the string 
 char *ioopm_strtok(char *src, char *delimiters, int *replacements)
 {
-
-
-
   /// Du får ändra på och kasta bort all nedanstående kod om du vill.
   /// OBS! Dessa variabler behåller sitt värde mellan funktionsanrop!
   static char *stored_src;       /// Används när src == NULL
-  static int stored_start;     /// Värdet på src första gånger
+  static char *stored_start;     /// Värdet på src första gånger
   static int replacement_index; /// Kan användas för att indexera replacements
   if (src)
     {
       /// Spara strängen
       stored_src = src;
-      stored_start = 0;
+      stored_start = src;
       replacement_index = 0;
     }
   else
     {
-      /// Om src == NULL, använd den sparade strängen
+      /// Om src == NULL, använd den sparade strängen 
       src = stored_src;
     }
-  int i = 0;
-  for (; ; i++) {
-    if (stored_src == NULL){
-      return(NULL);
-    }
-    if (src[i] == '\0'){
-      stored_src = NULL;
-      return(src);
-    }
-    if (!is_delimiter(src[i], delimiters)){
-      i++;
-    } else {
-      replacements[replacement_index*2] = src[i];
-      replacements[replacement_index*2+1] = i+stored_start;
-      replacement_index++;
-      src[i] = '\0';
-      //i++;
-      break;
-    }
-  }
-
-  for (; ; i++) {
-    if (src[i] == '\0') {
-      return(NULL);
-    }
-    if (is_delimiter(src[i], delimiters)){
-      i++;
-    } else {
-      stored_start += i;
-      stored_src = &src[i];
-      return(src);
-    }
-  }
 
   /// Write your code here!
 
-  /// Tips: du kan använda två loopar -- en för att skippa onödiga tecken i starten och en för att hitta slutet på tokenen
-
+  /// Tips: du kan använda två loopar -- en för att skippa onödiga tecken i starten och en för att hitta slutet på tokenen 
+  
   return NULL;
 }
