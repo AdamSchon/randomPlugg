9f71a5488e4e69605ce0eb05dec3d65453b6f0b2
AdamSchon <adam.schon.92@gmail.com>
2019-01-17 13:49:26 +0100

    123

--- PKDLabs/Lab2.hs
+++ PKDLabs/Lab2.hs
@@ -6,8 +6,9 @@ circleSquareArea x = circleArea x - squareArea(cathetus (2*x))
 
 
 rhymes first second =
-  if ((length first) > 3 && (length second) > 3) then
-    if (drop (length first - 3) == drop (length second - 3))
+  if ((length first) > 3 && (length second) > 3)
+  then
+    if (drop ((length first) - 3) == drop ((length second) - 3))
       then True
       else False
   else
