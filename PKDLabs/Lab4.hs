75a030a246a5aa7ae9e4b54e0e1a872c6faee222
AdamSchon <adam.schon.92@gmail.com>
2019-01-17 15:51:49 +0100

    123

--- PKDLabs/Lab4.hs
+++ PKDLabs/Lab4.hs
@@ -2,11 +2,9 @@
 
   stringOfInteger x =
     if ((abs x) > 9)
-      Then (stringOfInteger (div x 10)) ++ show(mod x 10)
-      else
-        if (x > 0)
-          then show(x)
-          else
-            if (x != 0)
-              then "-" ++ show(x)
-              else ""
+      then (stringOfInteger (div x 10)) ++ show(mod x 10)
+      else if (x > 0)
+            then show(x)
+            else if (x != 0)
+                  then "-" ++ show(x)
+                  else ""
