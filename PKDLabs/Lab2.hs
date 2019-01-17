15386519dfc2b3ddd363134f57dd923e3fa539a1
AdamSchon <adam.schon.92@gmail.com>
2019-01-17 13:37:37 +0100

    123

--- PKDLabs/Lab2.hs
+++ PKDLabs/Lab2.hs
@@ -3,3 +3,15 @@ circleArea x = (x^2)* pi
 squareCircleArea x = squareArea (2*x) - circleArea x
 cathetus x = x/sqrt 2
 circleSquareArea x = circleArea x - squareArea(cathetus (2*x))
+
+
+rhymes first second =
+if length first > 2 && length second > 2
+then
+  if drop (length first - 3) == drop (length second -3)
+    then True
+    else False
+  else
+    if first == second
+      then True
+      else False
