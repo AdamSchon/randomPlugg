f36571d285b0cb0d9d7c42b5a04ddc0267f16de1
AdamSchon <adam.schon.92@gmail.com>
2019-01-17 13:24:48 +0100

    123

--- PKDLabs/Lab2.hs
+++ PKDLabs/Lab2.hs
@@ -1,5 +1,5 @@
 squareArea x = (x^2)
 circleArea x = (x^2)* pi
-squareCircleArea x = squareArea x - circleArea x
+squareCircleArea x = squareArea (2*x) - circleArea x
 cathetus x = x/sqrt 2
 circleSquareArea x = circleArea x - squareArea(cathetus (2*x))
