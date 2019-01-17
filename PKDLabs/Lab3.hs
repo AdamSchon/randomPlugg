bf194d9a007d998e7880822f8f205fe7a8061a01
AdamSchon <adam.schon.92@gmail.com>
2019-01-17 15:08:18 +0100

    123

--- PKDLabs/Lab3.hs
+++ PKDLabs/Lab3.hs
@@ -3,6 +3,6 @@
 -- A better name of the function is "endsWith"(?)
 -- The arguments could be: Word and Ending
 
-updatePersonName (a, b, c, d, e) 1 new = (new,b,c,d,e)
-updatePersonName (a, b, c, d, e) 2 new = (a,new,c,d,e)
+updatePersonName (a, b, c, d, e) 2 new = (new,b,c,d,e)
+updatePersonName (a, b, c, d, e) 1 new = (a,new,c,d,e)
 updatePersonName (a, b, c, d, e) _ _ = (a,b,c,d,e)
