40eb5959a169e4d026542ae2c8eb35fe33296854
AdamSchon <adam.schon.92@gmail.com>
2019-01-18 15:05:52 +0100

    123

--- PKDLabs/Lab4.hs
+++ PKDLabs/Lab4.hs
@@ -19,3 +19,14 @@
           else recursiveSearch m s (n+1)
 
   searchString mainstring substring = recursiveSearch mainstring substring 0
+
+-- 1. vodalus is a String
+-- 2. "10" is not "". (1 * 2 + 0) 2 will be the output.
+-- 3. For each character in the string vodalus that is not "0" the function adds 2 to the output.
+-- 4. The output of severian is the amount of characters in vodalus multiplied by two.
+-- 5. Function name: DoubleNotZero. Variable name: string
+
+  myReplicate n x =
+    if (n == 0)
+      then xs
+      else x:myRepliace (n-1) x
