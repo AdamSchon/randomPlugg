3d535e07b4ff638a58a71c60a1dc2547841c34f4
AdamSchon <adam.schon.92@gmail.com>
2019-01-20 18:18:00 +0100

    123

--- PKDLabs/Lab5.hs
+++ PKDLabs/Lab5.hs
@@ -6,4 +6,4 @@ myInit list =
 fromDecimals list =
   if length list == 1
     then head list
-    else (head list) * (10 ^ (length list)) + (fromDecimals (drop 1 list))
+    else (head list) * (10 ^ (length list - 1)) + (fromDecimals (drop 1 list))
