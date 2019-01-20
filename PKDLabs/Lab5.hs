fa8a566a2fae451b084a9c25d6740812a6c893fb
AdamSchon <adam.schon.92@gmail.com>
2019-01-20 18:44:50 +0100

    123

--- PKDLabs/Lab5.hs
+++ PKDLabs/Lab5.hs
@@ -10,3 +10,10 @@ fromDecimals list =
       if length list == 1
         then head list
         else (head list) * (10 ^ (length list - 1)) + (fromDecimals (drop 1 list))
+
+squareOfEven list =
+  if list == []
+    then []
+    else
+      if ((head list) % 2 == 0)
+        then ((head list) ^ 2):squareOfEven (drop 1 list)
