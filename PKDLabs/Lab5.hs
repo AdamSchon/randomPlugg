ba37b69ab786126cf4b7d3bcb69b874e519de675
AdamSchon <adam.schon.92@gmail.com>
2019-01-20 18:12:55 +0100

    123

--- PKDLabs/Lab5.hs
+++ PKDLabs/Lab5.hs
@@ -4,6 +4,6 @@ myInit list =
     else (head list):myInit(drop 1 list)
 
 fromDecimals list =
-  if list == []
-    then 0
+  if length list == 1
+    then head list
     else (head list) * (10 ^ (length list)) + (fromDecimals (drop 1 list))
