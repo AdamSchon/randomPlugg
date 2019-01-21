979e4dcc6ce922839949caf744c28170dcb715e8
AdamSchon <adam.schon.92@gmail.com>
2019-01-21 12:46:31 +0100

    123

--- PKDLabs/Lab9.hs
+++ PKDLabs/Lab9.hs
@@ -2,4 +2,17 @@ sizeList list = map length list
 
 multiples n list = filter ((0==) . (\x -> mod x n)) list
 
-myConcat = foldr (++)
+myConcat = foldr (++) ""
+
+-- The function multiplies all the numbers in the list and returns the result.
+
+data Suite = Suite Hearts | Diamonds | Clubs | Spades deriving(Show)
+
+data Rank = Rank Int
+
+data Card = Card Suite Rank
+
+greaterCard (Card s1 r1) (Card s2 r2) =
+  if (r1 > r2)
+    then true
+    else false
