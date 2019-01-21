4c5bb0c903ce70b1a9b418fce9ce76fdf128bd3e
AdamSchon <adam.schon.92@gmail.com>
2019-01-21 12:50:25 +0100

    123

--- PKDLabs/Lab9.hs
+++ PKDLabs/Lab9.hs
@@ -4,4 +4,4 @@ multiples n list = filter ((0==) . (\x -> mod x n)) list
 
 myConcat = foldr (++) ""
 
--- The function multiplies all the numbers in the list and returns the result.
+-- The function multiplies all the numbers in the list and returns the result.  
