c78c4330c64ecf454aed6c7bb7ef9841e369e0f5
AdamSchon <adam.schon.92@gmail.com>
2019-01-21 12:51:59 +0100

    123

--- PKDLabs/Lab9.hs
+++ PKDLabs/Lab9.hs
@@ -2,6 +2,7 @@ sizeList list = map length list
 
 multiples n list = filter ((0==) . (\x -> mod x n)) list
 
+myConcat :: [String] -> String
 myConcat = foldr (++) ""
 
--- The function multiplies all the numbers in the list and returns the result.  
+-- The function multiplies all the numbers in the list and returns the result.
