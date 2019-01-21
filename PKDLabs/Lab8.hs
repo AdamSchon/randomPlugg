5423b85d41f992438a0dbbfdd7c6566a9d316f92
AdamSchon <adam.schon.92@gmail.com>
2019-01-21 10:50:04 +0100

    123

--- PKDLabs/Lab8.hs
+++ PKDLabs/Lab8.hs
@@ -1,4 +1,12 @@
--- head: [a] -> a
--- tail: [a] -> a
--- t -> t
--- 
+-- head: [a] -> a (POLY)
+-- tail: [a] -> a (POLY)
+-- t -> t (POLY)
+-- (,): a -> b ->(a,b) (POLY)
+-- (:): a -> [a] -> [a] (POLY)
+-- [[]]: [[t]] (POLY)
+-- tail [[]]: [[t]] (POLY)
+-- id:[] : [a -> a] (POLY)
+-- id id: a -> a (POLY)
+-- head [id] "foo": [char]
+
+flipArguments f a b = f b a
