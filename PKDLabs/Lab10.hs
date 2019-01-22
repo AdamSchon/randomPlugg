bf66e7f99be79651ca684dd7900572c9ec5b634f
AdamSchon <adam.schon.92@gmail.com>
2019-01-22 12:21:42 +0100

    123

--- PKDLabs/Lab10.hs
+++ PKDLabs/Lab10.hs
@@ -1,16 +1,16 @@
-data Suite = Hearts | Diamonds | Spades | Clubs deriving(Show)
+-- data Suite = Hearts | Diamonds | Spades | Clubs deriving(Show)
 
-data Card = Card Suite Int deriving(Show)
+data Card = Card String Int deriving(Show)
 
 greaterCard (Card s1 r1) (Card s2 r2) =
   if (r1 == r2)
     then if (s1 == s2)
       then False
-      else if (s1 == Spades && not(s2 == Spades))
+      else if (s1 == "Spades" && not(s2 == "Spades"))
         then True
-        else if (s1 == Hearts && (s2 == Clubs || s2 == Diamonds))
+        else if (s1 == "Hearts" && (s2 == "Clubs" || s2 == "Diamonds"))
           then True
-          else if (s1 == Clubs && s2 == Diamonds)
+          else if (s1 == "Clubs" && s2 == "Diamonds")
             then True
             else False
     else if (r1 > r2)
