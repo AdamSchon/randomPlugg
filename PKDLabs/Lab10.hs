1b7f35e97fc3f5e44ae627c351f96ddd213bf76a
AdamSchon <adam.schon.92@gmail.com>
2019-01-22 11:58:44 +0100

    123

--- PKDLabs/Lab10.hs
+++ PKDLabs/Lab10.hs
@@ -1,6 +1,8 @@
+data Suite = Hearts | Diamonds | Clubs | Spades deriving(Show)
+
 data Card = Hearts Int | Diamonds Int | Clubs Int | Spades Int deriving(Show)
 
-greaterCard (Card s1 r1) (Card s2 r2) =
+greaterCard (Suite s1 r1) (Suite s2 r2) =
   if (r1 > r2)
     then True
     else False
