6027478cc55b6bae1284c0b79de0c28230a6d56a
AdamSchon <adam.schon.92@gmail.com>
2019-01-23 19:10:40 +0100

    123

--- PKDLabs/Lab11.hs
+++ PKDLabs/Lab11.hs
@@ -18,7 +18,7 @@ findOlder (Just (s1, i1)) (Just(s2, i2)) =
     else Just (s2,i2)
 
 
-oldestWoman Void = Nothing
+--oldestWoman Void = Nothing
 oldestWoman (Woman name age left right) =
   findOlder (Just (name, age)) (findOlder ((oldestWoman left) (oldestWoman right)))
 oldestWoman (Man name age left right) = findOlder ((oldestWoman left) (oldestWoman right))
