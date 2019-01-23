a0511e9110c80916e674e0ff8ccf1cc83c596d04
AdamSchon <adam.schon.92@gmail.com>
2019-01-23 23:07:40 +0100

    123

--- PKDLabs/Lab14.hs
+++ PKDLabs/Lab14.hs
@@ -26,8 +26,8 @@ bad s n = printf "Bad \"%s\"\n%*s^\n " s (n+6) " "
 
 test1 = TestCase(assertEqual "test1: []" Nothing (isBalanced "[]"))
 test2 = TestCase(assertEqual "test2: [[[]]]" Nothing (isBalanced "[[[]]]"))
-test3 = TestCase(assertEqual "test3: []]" (Just 3) (isBalanced "[]]"))
+test3 = TestCase(assertEqual "test3: []]" (Just 2) (isBalanced "[]]"))
 test4 = TestCase(assertEqual "test 4: EmptyTest" Nothing (isBalanced ""))
-test5 = TestCase(assertEqual "test 5: [[[]]" (Just 5) (isBalanced "[[[]]"))
+test5 = TestCase(assertEqual "test 5: [[[]]" (Just 4) (isBalanced "[[[]]"))
 
 tests = TestList [TestLabel "test1" test1, TestLabel "test2" test2, TestLabel "test3" test3, TestLabel "test4" test4, TestLabel "test5" test5]
