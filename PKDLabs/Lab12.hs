41f01617481a823f252f84c009c5657e7fabfd80
AdamSchon <adam.schon.92@gmail.com>
2019-01-23 21:08:24 +0100

    123

--- PKDLabs/Lab12.hs
+++ PKDLabs/Lab12.hs
@@ -3,16 +3,30 @@ data BSTree = Void | Node BSTree Int BSTree deriving (Show)
 --findLargest (Node _ i Void) = i
 --findLargest (Node _ _ right) = findLargest right
 
-deleteMax (Node left i Void) = (i,left)
-deleteMax (Node left i right) = (z, Node left i tz)
-  where (z,tz) = deleteMax right
+--deleteMax (Node left i Void) = (i,left)
+--deleteMax (Node left i right) = (z, Node left i tz)
+--  where (z,tz) = deleteMax right
 
-delete (Node left i right) val
-  | i > val = (Node (delete left) i right)
-  | i < val = (Node left i (delete right))
-delete (Node Void i right) val = right
-delete (Node left o right) val = (Node tz z right)
-      where (z,tz) = deleteMax left
+--delete (Node left i right) val
+--  | i > val = (Node (delete left) i right)
+--  | i < val = (Node left i (delete right))
+--delete (Node Void i right) val = right
+--delete (Node left o right) val = (Node tz z right)
+--      where (z,tz) = deleteMax left
+
+deletemax :: (BSTree a) -> (a,BSTree a)
+deletemax (Node t1 y Nil) = (y,t1)
+deletemax (Node t1 y t2) = (z, Node t1 y tz)
+  where (z,tz) = deletemax t2
+
+deletetree :: (BSTree a) -> a -> (BSTree a)
+deletetree Void x = Void
+deletetree (Node tleft y tright) x
+  | x < y   = Node (deletetree tleft x) y tright
+  | x > y   = Node tleft y (deletetree tright x)
+deletetree (Node Void y tright) x   = tright
+deletetree (Node tleft y tright) x = Node tz z tright
+  where (z,tz) = deletemax tleft
 
 
 
