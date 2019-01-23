d3791d1f8ed6f535aca05c0299a6f186a87be859
AdamSchon <adam.schon.92@gmail.com>
2019-01-23 21:13:38 +0100

    123

--- PKDLabs/Lab12.hs
+++ PKDLabs/Lab12.hs
@@ -14,20 +14,19 @@ data BSTree = Void | Node BSTree Int BSTree deriving (Show)
 --delete (Node left o right) val = (Node tz z right)
 --      where (z,tz) = deleteMax left
 
---deletemax :: (BSTree a) -> (a,BSTree a)
+
 deletemax (Node t1 y Void) = (y,t1)
 deletemax (Node t1 y t2) = (z, Node t1 y tz)
   where (z,tz) = deletemax t2
 
---deletetree :: (BSTree a) -> a -> (BSTree a)
-deletetree Void x = Void
-deletetree (Node tleft y tright) x
-  | x < y   = Node (deletetree tleft x) y tright
-  | x > y   = Node tleft y (deletetree tright x)
-deletetree (Node Void y tright) x   = tright
-deletetree (Node tleft y tright) x = Node tz z tright
+delete Void x = Void
+delete (Node tleft y tright) x
+  | x < y   = Node (delete tleft x) y tright
+  | x > y   = Node tleft y (delete tright x)
+delete (Node Void y tright) x   = tright
+delete (Node tleft y tright) x = Node tz z tright
   where (z,tz) = deletemax tleft
 
 
 
--- deleteMax (Node (Node (Node Void 0 Void) 1 (Node Void 2 Void)) 3 (Node (Node (Node Void 4 Void) 5 (Node Void 6 Void)) 7 (Node Void 8 Void)))
+-- deletetree (Node (Node (Node Void 0 Void) 1 (Node Void 2 Void)) 3 (Node (Node (Node Void 4 Void) 5 (Node Void 6 Void)) 7 (Node Void 8 Void)))
