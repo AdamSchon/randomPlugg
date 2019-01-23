8f24a70539facf0c2f27a0d5588bd93d77a0b636
AdamSchon <adam.schon.92@gmail.com>
2019-01-23 13:31:51 +0100

    123

--- PKDLabs/Lab11.hs
+++ PKDLabs/Lab11.hs
@@ -11,10 +11,13 @@ data FamilyTree = Void
 findOlder Nothing Nothing = Nothing
 findOlder (s i) Nothing = (s i)
 findOlder Nothing (s i) = (s i)
-findOlder (s1 i1) (s2 i2) =
-  if (i1 > i2)
-    then (s1 i1)
-    else (s2 i2)
+findOlder (s1 i1) (s2 i2)
+          | i1 > i2 = (s1 i1)
+          | otherwise = (s2 i2)
+
+    merge (x:xs) (y:ys)
+      | y < x = y : merge (x:xs) ys
+      | otherwise = x : merge xs (y:ys)
 
 oldestWoman Void = Nothing
 oldestWoman (Man name age left right) =
