580558bf0a8608da49bdf92c7ff8394505edc5a3
AdamSchon <adam.schon.92@gmail.com>
2019-01-23 13:06:54 +0100

    123

---
+++ PKDLabs/Lab11.hs
@@ -0,0 +1,8 @@
+{ - The empty FamilyTree is given by void.
+  - A non-empty with gender a, name b, birth year c, left subtree l and right
+  subtree r is assigned through: a b c l r
+  -}
+
+data FamilyTree = Void deriving(Show)
+                | Man String Int FamilyTree FamilyTree deriving(Show)
+                | Woman String Int FamilyTree FamilyTree deriving(Show)
