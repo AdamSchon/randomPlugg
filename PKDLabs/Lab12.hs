7fd109ee4a5a4015fba37d4aa3c22390a7caec93
AdamSchon <adam.schon.92@gmail.com>
2019-01-23 20:57:23 +0100

    123

--- PKDLabs/Lab12.hs
+++ PKDLabs/Lab12.hs
@@ -3,6 +3,10 @@ data BSTree = Void | Node BSTree Int BSTree deriving (Show)
 --findLargest (Node _ i Void) = i
 --findLargest (Node _ _ right) = findLargest right
 
+deleteMax (Node left i Void) = (i,left)
+deleteMax (Node left i right) = (z, Node left i tz)
+  where (z,tz) = deleteMax right
+
 delete (Node left i right) val
   | i > val = (Node (delete left) i right)
   | i < val = (Node left i (delete right))
@@ -11,8 +15,6 @@ delete (Node Void i right) val   = right
 delete (Node left o right) val = Node tz z right
   where (z,tz) = deletemax left
 
-deleteMax (Node left i Void) = (i,left)
-deleteMax (Node left i right) = (z, Node left i tz)
-  where (z,tz) = deleteMax right
+
 
 -- deleteMax (Node (Node (Node Void 0 Void) 1 (Node Void 2 Void)) 3 (Node (Node (Node Void 4 Void) 5 (Node Void 6 Void)) 7 (Node Void 8 Void)))
