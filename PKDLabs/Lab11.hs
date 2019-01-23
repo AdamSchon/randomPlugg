eec50cd7d65c4baf2eba5a07afe058debacc18fc
AdamSchon <adam.schon.92@gmail.com>
2019-01-23 13:24:25 +0100

    123

--- PKDLabs/Lab11.hs
+++ PKDLabs/Lab11.hs
@@ -4,5 +4,16 @@
 --
 
 data FamilyTree = Void
-                | Man String Int FamilyTree FamilyTree 
+                | Man String Int FamilyTree FamilyTree
                 | Woman String Int FamilyTree FamilyTree deriving(Show)
+
+findOlder Nothing Nothing = nothing
+findOlder (s i) Nothing = (s i)
+findOlder Nothing (s i) = (s i)
+findOlder (s1 i1) (s2 i2) =
+  if (i1 > i2)
+    then (s1 i1)
+    else (s2 i2)
+
+oldestWoman Void = Nothing
+oldestWoman (Man name age left right) =
